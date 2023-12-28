aa7f1453c9ea3c1608901fef2ffc5ea10abe8d89
RyanQueirozS <ryanqueiroz@proton.me>
2023-12-28 19:30:06 -0300

    Made quests be stored in json files
        * Created a timer function that generates a time.Duration de-
         pendant of string size
        * Made Quest0 work with json and timer function

--- internal/game/quests/Quest0.go
+++ internal/game/quests/Quest0.go
@@ -4,6 +4,8 @@ import (
 	"log"
 	"tebu-discord/database/controller/save"
 	"tebu-discord/database/models"
+	"tebu-discord/pkg/dialog"
+	"tebu-discord/pkg/timer"
 	"time"
 
 	"github.com/bwmarrin/discordgo"
@@ -11,6 +13,7 @@ import (
 
 var (
 	progress uint8
+	dialogs  = dialog.GetDialog("quest_001")
 )
 
 func GenerateQuest0(
@@ -26,24 +29,24 @@ func GenerateQuest0(
 		},
 	})
 
+	paragraph := dialogs[0].DialogText[0]
 	time.Sleep(time.Second * 1) // Value I found to be long enough to start quest but on to long
 	msg, err := s.FollowupMessageCreate(i.Interaction, true, &discordgo.WebhookParams{
 		Flags:   discordgo.MessageFlagsEphemeral,
-		Content: "All of a sudden, a bright blue light comes shinning at your eyes, illuminating a canvas of clouds that dance in the skies to the most beautiful melody. The air hums in nature's sweet symphony, you catch the invigorating scent of fresh grass.",
+		Content: paragraph,
 	})
 	if err != nil {
 		log.Fatalf("Error creating follow up messsage: %v", err)
 	}
 
-	time.Sleep(time.Second * 1)
+	time.Sleep(timer.GenerateQuestTime(paragraph))
 
-	paragraph2 := "All of a sudden, a bright blue light comes shinning at your eyes, illuminating a canvas of clouds that dance in the skies to the most beautiful melody. The air hums in nature's sweet symphony, you catch the invigorating scent of fresh grass.\nGetting up, the landscape unfolds before you, a vast expanse of lush green plains stretching beyond the horizon. The world flares out like a blank sheet of paper, inviting you to explore its mysteries and secrets."
+	paragraph = dialogs[1].DialogText[0]
 
 	s.FollowupMessageEdit(i.Interaction, msg.ID, &discordgo.WebhookEdit{
-		Content: &paragraph2,
+		Content: &paragraph,
 	})
-
-	time.Sleep(time.Second * 1)
+	time.Sleep(timer.GenerateQuestTime(paragraph))
 
 	s.FollowupMessageCreate(i.Interaction, true, &discordgo.WebhookParams{
 		Flags: discordgo.MessageFlagsEphemeral,
@@ -73,25 +76,25 @@ func ButtonQuest0(
 
 	cloudSave, err := save.GetSave(i.User.ID)
 	if err != nil {
-
 	}
 	progress++
 	switch progress {
 	case 1:
-		label = "Explore"
-		content = "You run towards the sun, but all you find is an endless sea of grass"
+		// The 0ith and 1th is used in the function above
+		label = dialogs[2].ButtonLabel[0]
+		content = dialogs[2].DialogText[0]
 	case 2:
-		label = "Explore"
-		content = "Nothing new seems to appear at your sight"
+		label = dialogs[3].ButtonLabel[0]
+		content = dialogs[3].DialogText[0]
 	case 3:
-		label = "Explore"
-		content = "Wind rattles and shakes the vegetation around you, very far something different catches your attention"
+		label = dialogs[4].ButtonLabel[0]
+		content = dialogs[4].DialogText[0]
 	case 4:
-		label = "Climb tree"
-		content = "A majestic tall tree, rooted to the ever so green grass, you notice rocks shattered within it's roots, while fungi grows within the gap of the base of the tree and the soil."
+		label = dialogs[5].ButtonLabel[0]
+		content = dialogs[5].DialogText[0]
 	case 5:
-		label = "Climb down"
-		content = "Scalling the towering tree, in a distance, you see other lonely trees, none as tall as the one you are in. The shrub land seemed to be getting denser the further away you look"
+		label = dialogs[6].ButtonLabel[0]
+		content = dialogs[6].DialogText[0]
 
 		cloudSave.Progress.Quest.QuestNumber = 1
 		save.UpdateSave(cloudSave)
---
+++ internal/models/models.go
@@ -0,0 +1,6 @@
+package models
+
+type Dialog struct {
+	ButtonLabel []string `json:"button_label"`
+	DialogText  []string `json:"dialog_text"`
+}
